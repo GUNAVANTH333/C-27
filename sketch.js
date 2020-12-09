b2f3274985bfacef53486acaeca301e20e0354c1
gunavanth <guna2006.3@gmail.com>
2020-12-09 13:46:53 +0530

    abc

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
@@ -35,6 +36,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6= new Log (230,180,80,PI/2);
+
+    chain1 = new Chain(bird.body,log6.body)
 
 }
 
@@ -60,5 +64,7 @@ function draw(){
     log5.display();
 
     bird.display();
+    log6.display();
     platform.display();
+    chain1.display();
 }
